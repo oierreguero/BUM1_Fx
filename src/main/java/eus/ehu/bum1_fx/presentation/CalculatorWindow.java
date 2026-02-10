1f499c3796f85153ddde2b255dffde47713f1c16
oierreguero <oierreguero1109@gmail.com>
2026-02-10 11:11:22 +0100

    Final2

--- src/main/java/eus/ehu/bum1_fx/presentation/CalculatorWindow.java
+++ src/main/java/eus/ehu/bum1_fx/presentation/CalculatorWindow.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class CalculatorWindow extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CalculatorWindow.class.getResource("calculator.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(CalculatorWindow.class.getResource("/eus/ehu/bum1_fx/calculatorWindow.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 305);
         stage.setTitle("Currency Exchange Calculator");
         stage.setScene(scene);
